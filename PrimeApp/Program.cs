5ff442550164af31876327c42077ff8b4c339b59
Kate <ponkrkat@gmail.com>
2018-10-31 14:04:42 +0700

    second name changed

---
+++ PrimeApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PrimeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var srv = new Prime.Services.PrimeService();
+
+            var prim = srv.PrimeName("Ponkratyeva");
+
+            Console.WriteLine($"PrimeName: {prim}");
+        }
+    }
+}
